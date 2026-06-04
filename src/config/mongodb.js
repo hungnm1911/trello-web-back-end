1dde5a666a1d028f43f0349198b50db12ac85e4a
Hung HeHe <manhung19112003@gmail.com>
2026-06-04 16:25:30 +0700

    Thiết lập kết nối MongoDB + Thiết lập biến env

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,56 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
  * "A bit of fragrance clings to the hand that gives flowers!"
  */
+
+import { env } from '~/config/environment.js'
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+
+// khởi tạo đối tượng trelloDatabaseInstance ban đầu là null (vì chúng ta chưa kết nối đến cơ sở dữ liệu)
+let trelloDatabaseInstance = null
+
+// Khởi tạo đối tượng mongoClientInstance để kết nối đến MongoDB bằng URI và cấu hình server API
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// kết nối tới Database
+export const CONNECT_DB = async () => {
+  try {
+    // Kết nối đến MongoDB Atlas với URI đã khai báo trong mongoClientInstance
+    await mongoClientInstance.connect()
+    console.log('Connected to MongoDB successfully!')
+
+    // kết nối thành công thì lấy ra Database theo tên và gãn ngược lại vào biến trelloDatabaseInstance
+    trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+  } catch (error) {
+    console.error('Error connecting to MongoDB:', error)
+    throw error
+  }
+}
+
+// Function GET_DB (không async) có nhiệm vụ export ra cái Trello Database Insstance sau khi đã kết nối thành công để sử dụng nhiều nơi khác nhau trong code
+// Lưu ý phải đảm bảo luôn gọi getDB này sau khi kết nối thành công với CONNECT_DB, nếu không sẽ bị lỗi vì trelloDatabaseInstance vẫn là null
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) {
+    throw new Error('Database not connected. Please call CONNECT_DB first.')
+  }
+  return trelloDatabaseInstance
+}
+
+export const CLOSE_DB = async () => {
+  try {
+    console.log('Closing MongoDB connection...')
+    await mongoClientInstance.close()
+  } catch (error) {
+    console.error('Error closing MongoDB connection:', error)
+    throw error
+  }
+}
--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * Updated by trungquandev.com's author on August 17 2023
  * YouTube: https://youtube.com/@trungquandev
@@ -5,29 +6,53 @@
  */
 
 import express from 'express'
-import { mapOrder } from '~/utils/sorts.js'
+import { CONNECT_DB, CLOSE_DB } from '~/config/mongodb.js'
+import exitHook from 'async-exit-hook'
+import { env } from '~/config/environment.js'
 
-const app = express()
+const START_SERVER = () => {
+  const app = express()
 
-const hostname = 'localhost'
-const port = 8017
+  app.get('/', (req, res) => {
+    res.end(`Hello ${env.AUTHOR}, I am running at /`)
+  })
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  // eslint-disable-next-line no-console
-  console.log(mapOrder(
-    [{ id: 'id-1', name: 'One' },
-      { id: 'id-2', name: 'Two' },
-      { id: 'id-3', name: 'Three' },
-      { id: 'id-4', name: 'Four' },
-      { id: 'id-5', name: 'Five' } ],
-    ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-    'id'
-  ))
-  res.end('<h1>Hello World!</h1><hr>')
-})
+  app.listen(env.APP_PORT, env.APP_HOST, async () => {
+    // eslint-disable-next-line no-console
+    console.log(`3. Hello '${env.AUTHOR}', I am running at ${ env.APP_HOST }:${ env.APP_PORT }/`)
+  })
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello Trung Quan Dev, I am running at ${ hostname }:${ port }/`)
-})
+  // Thực hiện các tác vụ cleanup trước khi dừng server bằng bất cứ thao tác nào
+  exitHook(() => {
+    console.log('4. Server is shutting down...')
+    CLOSE_DB()
+    console.log('5. Disconnected from MongoDB Atlas!!!')
+  })
+}
+
+// Chỉ khi kết nối tới DB thành công thì mới Start server back-end lên
+(async () => {
+  try {
+    console.log('1. Connecting to MongoDB Atlas...')
+    await CONNECT_DB()
+    console.log('2. Connected to MongoDB Cloud Atlas!!!')
+    START_SERVER()
+  } catch (error) {
+    console.error(error)
+    process.exit(1) // thoát chương trình với mã lỗi 1 nếu có lỗi xảy ra trong quá trình kết nối hoặc khởi động server
+  }
+})()
+
+// // Chỉ khi kết nối tới DB thành công thì mới Start server back-end lên
+// console.log('1. Connecting to MongoDB Atlas...')
+// CONNECT_DB()
+//   .then(() => console.log('2. Connected to MongoDB Cloud Atlas!!!'))
+//   .then(() => START_SERVER())
+//   .catch(error => {
+//     console.error(error)
+//     process.exit(1) // thoát chương trình với mã lỗi 1 nếu có lỗi xảy ra trong quá trình kết nối hoặc khởi động server
+//   })
+
+
+// L : Khi chạy yarn dev, code luôn chạy từ file server.js này đầu tiên \
+// => chỉ cần import module cần thiết (ví dụ dotenv) vào đây thì toàn bộ project sẽ có thể sử dụng được rồi, không cần phải import lại ở những file khác nữa
